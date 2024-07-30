4c85320f13921f8c05e7da18110bfa5bdc8e457d
Dev <guru01803@gmail.com>
2024-07-30 17:44:15 +0530

    setup backend and started server

---
+++ server/Utils/Db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const DbConnection=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('Connected to Database');
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+module.exports=DbConnection;
---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express=require('express')
+const cors=require('cors')
+const cookieParser=require('cookie-parser')
+const DbConnection=require('./Utils/Db')
+const app=express();
+
+DbConnection();
+
+app.use(express.json());
+app.use(cors({
+    origin:["http://localhost:3000"],
+    methods:["GET","POST","DELETE","PUT"],
+    credentials:true,
+    optionsSuccessStatus:204
+}));
+
+app.use(cookieParser());
+
+app.listen(process.env.PORT,()=>{
+    console.log('Listening on port',process.env.PORT)
+})
